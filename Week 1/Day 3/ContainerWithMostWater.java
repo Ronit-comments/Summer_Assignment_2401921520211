d3d8404d47d25f206b321ea9309bf9211b1832d7
Ronit-comments <rsoni310706@gmail.com>
2026-06-03 18:44:07 +0530

    Add files via upload

---
+++ Week 1/Day 3/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int maxArea = 0;
+        while(left < right){
+            int water = Math.min(height[left] , height[right]) * (right - left) ;
+            maxArea = Math.max(water , maxArea) ;
+            if(height[left]<height[right]){
+                left++;
+            } 
+            else{
+                right-- ;
+            }
+        }
+        return maxArea ;
+    }
+}
